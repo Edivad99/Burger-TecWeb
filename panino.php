e3ab5f2ce17575dde6f80212a82e1cfb8fedb51f
Davide Albiero <davide2910@live.it>
2020-12-06 20:47:23 +0100

    Cambiato i segnaposto

--- panino.php
+++ panino.php
@@ -1,9 +1,9 @@
 <?php
 $paginaHTML = file_get_contents("html/panino.html");
 
-$paginaHTML = str_replace("{{ %nomePanino }}", "Crunchicken", $paginaHTML);
-$paginaHTML = str_replace("{{ %immaginePanino }}", "img/pollo/crunchicken.png", $paginaHTML);
-$paginaHTML = str_replace("{{ %altImmaginePanino }}", "AGGIUNGI UN ALT", $paginaHTML);
+$paginaHTML = str_replace("{{ nomePanino }}", "Crunchicken", $paginaHTML);
+$paginaHTML = str_replace("{{ immaginePanino }}", "img/pollo/crunchicken.png", $paginaHTML);
+$paginaHTML = str_replace("{{ altImmaginePanino }}", "AGGIUNGI UN ALT", $paginaHTML);
 
 //Crea un metodo che faccia questo
 
