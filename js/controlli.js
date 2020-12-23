9c6cdc92ff43e463610e13b50a4e179a2c5da874
Davide Albiero <davide2910@live.it>
2020-12-23 18:32:17 +0100

    fixed #42

--- js/controlli.js
+++ js/controlli.js
@@ -24,6 +24,7 @@ function validaCommento() {
 }
 
 function validaFormEvento() {
+    document.getElementById("datiNonCorretti").remove();
     let titolo = document.getElementById("titolo").value.trim();
     let data = document.getElementById("data").value;
     let luogo = document.getElementById("luogo").value.trim();
