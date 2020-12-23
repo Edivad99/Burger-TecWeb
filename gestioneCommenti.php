da8d0a7c06861b92d90169c3ed77399ee5d7c454
Matteo Tossuto <matteotossuto@gmail.com>
2020-12-23 20:09:43 +0100

    Commit per apertura Branch
    Ho creato una prima query per pescare i commenti dal DB. Bisogna implementare un controllo, probabilmente tramite JS per poter caricare ogni volta nuovi commenti.

--- gestioneCommenti.php
+++ gestioneCommenti.php
@@ -7,11 +7,38 @@ use DB\DBAccess;
 
 session_start();
 if(!isset($_SESSION["isValid"]) || !$_SESSION["isValid"]) {
-    header("Location: index.php");
+    header("Location: login.php");
+}
+
+$dbAccess = new DBAccess();
+$connessioneRiuscita = $dbAccess->openDBConnection();
+if(!$connessioneRiuscita) {
+    header("Location: error_500.php");
+    die;
+}
+
+$commenti = $dbAccess->getCommenti();
+$dbAccess->closeDBConnection();
+
+$listaCommenti="";
+if(count($commenti) > 0) {
+    $patternCommento = file_get_contents("html/components/commento.html");
+    foreach($commenti as $commento) {
+        $content = array(
+            "{{ username }}" => $commento["Username"],
+            "{{ dataOraPost }}" => $commento["DataOraPost"],
+            "{{ contenuto }}" => $commento["Contenuto"]
+        );
+
+        $listaCommenti .= Util::replacerFromHTML($patternCommento, $content);
+    }
+} else {
+    $listaCommenti = "NON CI SONO COMMENTI";
 }
 
 $content = array(
-    "{{ username }}" => $_SESSION["username"]
+    "{{ username }}" => $_SESSION["username"],
+    "<commenti/>" => $listaCommenti
 );
 
 echo Util::replacer("html/gestioneCommenti.html", $content);
