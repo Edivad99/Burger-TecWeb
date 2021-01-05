79533a056a6d6856018880d30dc6a46f887fb20b
Davide Albiero <davide2910@live.it>
2021-01-05 16:35:09 +0100

    Sistemato cambio password errori

--- cambioPassword.php
+++ cambioPassword.php
@@ -10,18 +10,8 @@ if(!isset($_SESSION["isValid"]) || !$_SESSION["isValid"]) {
 }
 
 $msg = "";
-if(isset($_GET["cambioAvvenuto"]) && $_GET["cambioAvvenuto"] == 1) {
-    //Cambio avvenuto con successo
-    $msg = "<p id=\"cambioAvvenuto\">La modifica della password è avvenuta con successo!</p>";
-} else if(isset($_GET["errConferma"]) && $_GET["errConferma"] == 1) {
-    //Errore nella nuova password e sua conferma
-    $msg = "<p id=\"datiNonCorretti\">La nuova password e quella di conferma non corrispondono</p>";
-} else if(isset($_GET["errNuovaPwd"]) && $_GET["errNuovaPwd"] == 1) {
-    //Errore nella nuova password, è uguale alla vecchia
-    $msg = "<p id=\"datiNonCorretti\">La nuova password è uguale a quella vecchia</p>";
-} else if(isset($_GET["errDati"]) && $_GET["errDati"] == 1) {
-    //Errore inserimento dei dati user e password
-    $msg = "<p id=\"datiNonCorretti\">La vecchia password non è corretta</p>";
+if(isset($_GET["messaggio"])) {
+    $msg = "<p id=\"datiNonCorretti\">" . $_GET["messaggio"] . "</p>";
 }
 
 $content = array(
